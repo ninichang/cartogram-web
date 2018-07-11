61e61b21bd857269616f3240b551dc1f53f3e0a0
Ian Duncan <janskykd@gmail.com>
2018-07-11 13:11:12 +0800

    added missing stylesheet; updated gunicorn script

--- internal/gunicorn.sh
+++ internal/gunicorn.sh
@@ -5,4 +5,8 @@ then
     echo "Warning: You are running a production server in development mode. Is this what you want?"
 fi
 
-gunicorn --bind $CARTOGRAM_HOST:$CARTOGRAM_PORT web:app
+WORKERS=$(echo `cat /proc/cpuinfo | grep -c processor` "*2 + 1" | bc)
+
+echo "Starting gunicorn in daemon mode with " $WORKERS " worker(s)."
+
+gunicorn --bind $CARTOGRAM_HOST:$CARTOGRAM_PORT -w $WORKERS -D web:app
