19eb6539c3d18c147b9de35fa289c30a8df6e1d2
Ian Duncan <janskykd@gmail.com>
2019-06-25 13:44:59 +0800

    remove australia map due to too few points

--- internal/web.py
+++ internal/web.py
@@ -4,7 +4,6 @@ from handlers import usa, india, china, germany, brazil
 
 # !!!DO NOT MODFIY THE FOLLOWING SECTION
 from handlers import srilanka
-from handlers import australia
 # ---addmap.py header marker---
 # !!!END DO NOT MODFIY
 
@@ -50,7 +49,6 @@ cartogram_handlers = {
     'brazil': brazil.CartogramHandler(),
 # !!!DO NOT MODFIY THE FOLLOWING SECTION
 'srilanka': srilanka.CartogramHandler(),
-'australia': australia.CartogramHandler(),
 # ---addmap.py body marker---
 # !!!END DO NOT MODFIY
 }
